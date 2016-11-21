4035c7c74c0b3332fe94edc9f38309f180d9417d
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-11-22 01:11:09 +0900

    Add withdrawals#create action.

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -4,4 +4,17 @@ class WithdrawalsController < ApplicationController
     @withdrawal = Withdrawal.new
   end
 
+  def create
+    @withdrawal = Withdrawal.new(withdrawal_params)
+    if @withdrawal.save
+    else
+      render :new
+    end
+  end
+
+  private
+  def withdrawal_params
+    params.require(:withdrawal).permit(:impression_unuseful, :impression_difficult, :impression_not_enough)
+  end
+
 end
