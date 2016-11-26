cba3e23c568cf4298068cf12da237c5220cae7ff
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-11-26 16:03:45 +0900

    bundle exec annotate

--- app/models/withdrawal.rb
+++ app/models/withdrawal.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: withdrawals
+#
+#  id                    :integer          not null, primary key
+#  impression_unuseful   :boolean          default(FALSE), not null
+#  impression_difficult  :boolean          default(FALSE), not null
+#  impression_not_enough :boolean          default(FALSE), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
 class Withdrawal < ActiveRecord::Base
   attr_accessor :impression
   validates :impression, present_impression: true
--- test/models/withdrawal_test.rb
+++ test/models/withdrawal_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: withdrawals
+#
+#  id                    :integer          not null, primary key
+#  impression_unuseful   :boolean          default(FALSE), not null
+#  impression_difficult  :boolean          default(FALSE), not null
+#  impression_not_enough :boolean          default(FALSE), not null
+#  created_at            :datetime         not null
+#  updated_at            :datetime         not null
+#
+
 require 'test_helper'
 
 class WithdrawalTest < ActiveSupport::TestCase
