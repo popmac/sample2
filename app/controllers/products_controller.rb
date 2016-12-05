e63f44ccc34e24ab047514b7d9cd7a972d1cdc07
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-05 22:52:14 +0900

    productを作成した時にuser_idも保存されるようにした。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :content)
+      params.require(:product).permit(:name, :content).merge(user_id: current_user.id)
     end
 end
