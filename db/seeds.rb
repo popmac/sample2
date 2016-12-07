f374b33335525af21236c87ab8ca0392e6aad724
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-07 14:08:55 +0900

    userとproductのfixtureを追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(email: "hoge1@hoge.com",
+             password:              "aaaa1111",
+             password_confirmation: "aaaa1111")
+
+5.times do |n|
+  email = "hoge#{n+2}@hoge.com"
+  User.create!(email: email,
+               password:              "aaaa1111",
+               password_confirmation: "aaaa1111")
+end
+
+Product.create!(name: "atom",
+                content: "最高かよ",
+                user_id: 1)
+
+Product.create!(name: "slack",
+                content: "慣れると使いやすい",
+                user_id: 2)
