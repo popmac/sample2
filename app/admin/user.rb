c3c66099233fb61d32991efd3203c08b987b5138
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-08 19:35:44 +0900

    userをadmin管理画面で作成できるようにした。

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -13,5 +13,14 @@ ActiveAdmin.register User do
 #   permitted
 # end
 
+permit_params :email, :password
+
+form do |f|
+  f.inputs do
+    f.input :email
+    f.input :password
+  end
+  f.actions
+end
 
 end
