53e2c6d1ad28944efc552ffd5e13dc0735e56733
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2016-12-08 22:40:21 +0900

    インデント調整とコメントアウトを追加した。

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,53 +1,45 @@
 ActiveAdmin.register Product do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-controller do
-  def scoped_collection
-    Product.includes(:user)
-  end
-end
-
-index do
-  selectable_column
-
-  column :id do |product|
-    link_to product.id, admin_product_path(product)
-  end
-  column :name
-  column :content
-  column :user_id do |product|
-    if product.user.present?
-      link_to product.user.id, admin_user_path(product.user)
-    else
-      status_tag('Empty')
+  # N+1問題を解決
+  controller do
+    def scoped_collection
+      Product.includes(:user)
     end
   end
-  column :created_at
-  column :updated_at
-  actions
-end
 
-permit_params :name, :content, :user_id
+  # 登録されたデータ一覧が表示される画面をカスタマイズ
+  index do
+    selectable_column
 
-form do |f|
-  f.inputs do
-    f.input :user, :as => :select, :member_label => :id, :label => 'ユーザーID'
-    f.input :name
-    f.input :content
+    column :id do |product|
+      link_to product.id, admin_product_path(product)
+    end
+    column :name
+    column :content
+    column :user_id do |product|
+      if product.user.present?
+        link_to product.user.id, admin_user_path(product.user)
+      else
+        status_tag('Empty')
+      end
+    end
+    column :created_at
+    column :updated_at
+    actions
   end
-  f.actions
-end
+
+  # 管理画面からProductのデータを登録できるようにする
+  permit_params :name, :content, :user_id
+
+  # Productのデータを登録するフォームをカスタマイズ
+  form do |f|
+    f.inputs do
+      # user_idを選択できるようにしている
+      f.input :user, :as => :select, :member_label => :id, :label => 'ユーザーID'
+      f.input :name
+      f.input :content
+    end
+    f.actions
+  end
 
 end
