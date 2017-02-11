07d945452061b4129a75561c49777bcac411ab2e
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-02-12 00:38:13 +0900

    seedを修正した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@ User.create!(email: "hoge1@hoge.com",
                password_confirmation: "aaaa1111")
 end
 
-Product.create!(name: "atom",
-                content: "最高かよ",
-                user_id: 1)
-
-Product.create!(name: "slack",
-                content: "慣れると使いやすい",
-                user_id: 2)
+# Product.create!(name: "atom",
+#                 content: "最高かよ",
+#                 user_id: 1)
+#
+# Product.create!(name: "slack",
+#                 content: "慣れると使いやすい",
+#                 user_id: 2)
 
 AdminUser.create!(email: 'hoge1@hoge.com', password: 'aaaa1111', password_confirmation: 'aaaa1111')
