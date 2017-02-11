8c11619e77f329c91e25fff2727dc0dfa6618261
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-02-11 18:46:04 +0900

    Productのurlを投稿できるようにした。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :content).merge(user_id: current_user.id)
+      params.require(:product).permit(:name, :url, :content).merge(user_id: current_user.id)
     end
 end
