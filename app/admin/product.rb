1138cc00e456d815ffbcb444c91739afba2da26d
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-02-12 22:20:59 +0900

    Admin画面でproductを作成する際に、url先のtitleを取得して保存するようにした。

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -5,6 +5,17 @@ ActiveAdmin.register Product do
     def scoped_collection
       Product.includes(:user)
     end
+    def create
+      @product = Product.new(params.require(:product).permit(:name, :url, :content).merge(user_id: current_user.id))
+      url = @product.url
+      agent = Mechanize.new
+      page = agent.get("#{url}")
+      element = page.at('head title')
+      title = element.inner_text
+      @product.title = title
+      @product.save
+      redirect_to admin_product_path(@product)
+    end
   end
 
   # 登録されたデータ一覧が表示される画面をカスタマイズ
