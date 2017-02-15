7a835e588e0eea2132905c961da2b8ce091b99cc
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-02-15 17:14:29 +0900

    Productsテーブルからurlカラムを削除した。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214125255) do
+ActiveRecord::Schema.define(version: 20170215081331) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 20170214125255) do
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
     t.integer  "user_id",     limit: 4
-    t.text     "url",         limit: 65535
     t.text     "title",       limit: 65535
     t.text     "image",       limit: 65535
     t.text     "picture_url", limit: 65535
